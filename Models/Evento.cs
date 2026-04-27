a9aa45692677c463f41058efbab285bd078d22a0
luisantonioea <lantonioespinoza10@gmail.com>
2026-04-26 22:12:19 -0500

    funcionalidad 1: Catálogo de Eventos

--- Models/Evento.cs
+++ Models/Evento.cs
@@ -1,24 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace PaloGames.Models
 {
     public class Evento
     {
         public int Id { get; set; }
-
-        public string Nombre { get; set; } = "";
-        public string Descripcion { get; set; } = "";
-
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Categoria { get; set; } // Hackatons, Conciertos, Ferias, etc.
         public DateTime Fecha { get; set; }
-        public string Lugar { get; set; } = "";
-
-        public string ImagenUrl { get; set; } = "";
-
-        public decimal PrecioBase { get; set; }
-
-        public int CapacidadTotal { get; set; }
+        public string Ubicacion { get; set; }
+        public decimal Costo { get; set; } 
+        public string ImagenUrl { get; set; }
     }
 }
